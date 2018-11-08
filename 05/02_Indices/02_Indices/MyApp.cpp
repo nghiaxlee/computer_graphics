e40a8eab08a4f4a1893d01055b9967cf993ea4bc
Le Minh Nghia <aaogmu@INF.ELTE.HU>
2018-11-08 17:46:54 +0100

    Lab07

---
+++ 05/02_Indices/02_Indices/MyApp.cpp
@@ -0,0 +1,189 @@
+#include "MyApp.h"
+#include "GLUtils.hpp"
+
+#include <math.h>
+
+CMyApp::CMyApp(void)
+{
+	m_vaoID = 0;
+	m_vboID = 0;
+	m_ibID = 0;
+
+	m_programID = 0;
+}
+
+
+CMyApp::~CMyApp(void)
+{
+}
+
+bool CMyApp::Init()
+{
+	glClearColor(0.125f, 0.25f, 0.5f, 1.0f);
+
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+	glCullFace(GL_BACK);
+
+	Vertex vert[] =
+	{ 
+		//          x,  y, z             R, G, B
+		{glm::vec3(-1, -1, 0), glm::vec3(1, 0, 0)},
+		{glm::vec3( 1, -1, 0), glm::vec3(0, 1, 0)},
+		{glm::vec3(-1,  1, 0), glm::vec3(0, 0, 1)},
+		{glm::vec3( 1,  1, 0), glm::vec3(1, 1, 1)},
+	};
+
+	// index buffer
+    GLushort indices[]=
+    {
+		// 1st triangle
+        0,1,2,
+		// 2nd triangle
+        2,1,3,
+    };
+
+	glGenVertexArrays(1, &m_vaoID);
+	glBindVertexArray(m_vaoID);
+	
+	glGenBuffers(1, &m_vboID); 
+	glBindBuffer(GL_ARRAY_BUFFER, m_vboID);
+	glBufferData( GL_ARRAY_BUFFER, sizeof(vert), vert, GL_STATIC_DRAW);
+	
+	glEnableVertexAttribArray(0); 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),	0); 
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(glm::vec3)) );
+
+	// create index buffer
+	glGenBuffers(1, &m_ibID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glBindVertexArray(0); 
+	glBindBuffer(GL_ARRAY_BUFFER, 0); 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); 
+
+	GLuint vs_ID = loadShader(GL_VERTEX_SHADER,		"myVert.vert");
+	GLuint fs_ID = loadShader(GL_FRAGMENT_SHADER,	"myFrag.frag");
+
+	m_programID = glCreateProgram();
+
+	glAttachShader(m_programID, vs_ID);
+	glAttachShader(m_programID, fs_ID);
+
+	glBindAttribLocation(	m_programID, 0, "vs_in_pos");
+	glBindAttribLocation( m_programID, 1, "vs_in_col");
+
+	glLinkProgram(m_programID);
+
+	GLint infoLogLength = 0, result = 0;
+
+	glGetProgramiv(m_programID, GL_LINK_STATUS, &result);
+	glGetProgramiv(m_programID, GL_INFO_LOG_LENGTH, &infoLogLength);
+	if ( GL_FALSE == result )
+	{
+		std::vector<char> ProgramErrorMessage( infoLogLength );
+		glGetProgramInfoLog(m_programID, infoLogLength, NULL, &ProgramErrorMessage[0]);
+		fprintf(stdout, "%s\n", &ProgramErrorMessage[0]);
+		
+		char* aSzoveg = new char[ProgramErrorMessage.size()];
+		memcpy( aSzoveg, &ProgramErrorMessage[0], ProgramErrorMessage.size());
+
+		std::cout << "[app.Init()] Sáder Huba panasza: " << aSzoveg << std::endl;
+
+		delete aSzoveg;
+	}
+
+	glDeleteShader( vs_ID );
+	glDeleteShader( fs_ID );
+
+	m_matProj = glm::perspective( 45.0f, 640/480.0f, 1.0f, 1000.0f );
+
+	m_loc_mvp = glGetUniformLocation( m_programID, "MVP");
+
+	return true;
+}
+
+void CMyApp::Clean()
+{
+	glDeleteBuffers(1, &m_vboID);
+	glDeleteBuffers(1, &m_ibID);
+	glDeleteVertexArrays(1, &m_vaoID);
+
+	glDeleteProgram( m_programID );
+}
+
+void CMyApp::Update()
+{
+	m_matView = glm::lookAt(glm::vec3( 0,  0,  5),		// eye
+							glm::vec3( 0,  0,  0),		// look at
+							glm::vec3( 0,  1,  0));		// up
+}
+
+
+void CMyApp::Render()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram( m_programID );
+
+	// set shader uniforms' values
+	/*
+
+	GLM transformation matrices
+		glm::rotate<float>( angle, axis_x, axis_y, axis_z) 
+		glm::translate<float>( d_x, d_y, d_z) 
+		glm::scale<float>( s_x, s_y, s_z )
+
+	*/
+	m_matWorld = glm::mat4(1.0f);
+
+	glm::mat4 mvp = m_matProj * m_matView * m_matWorld;
+
+	glUniformMatrix4fv( m_loc_mvp, 1, GL_FALSE, &(mvp[0][0]) );
+
+	glBindVertexArray(m_vaoID);
+
+	glDrawElements(	GL_TRIANGLES,		// primitive type
+					6,					// number of vertices used for the draw call
+					GL_UNSIGNED_SHORT,	// index type
+					0);					// index location
+
+	glBindVertexArray(0);
+
+	glUseProgram( 0 );
+}
+
+void CMyApp::KeyboardDown(SDL_KeyboardEvent& key)
+{
+}
+
+void CMyApp::KeyboardUp(SDL_KeyboardEvent& key)
+{
+}
+
+void CMyApp::MouseMove(SDL_MouseMotionEvent& mouse)
+{
+
+}
+
+void CMyApp::MouseDown(SDL_MouseButtonEvent& mouse)
+{
+}
+
+void CMyApp::MouseUp(SDL_MouseButtonEvent& mouse)
+{
+}
+
+void CMyApp::MouseWheel(SDL_MouseWheelEvent& wheel)
+{
+}
+
+void CMyApp::Resize(int _w, int _h)
+{
+	glViewport(0, 0, _w, _h);
+
+	m_matProj = glm::perspective(  45.0f, _w/(float)_h,	0.01f, 100.0f);
+}
